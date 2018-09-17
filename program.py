f4963e63248501851fb7dcc5841e1bf5c62d6f84
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-09-17 11:07:07 -0400

    add program.py

--- program.py
+++ program.py
@@ -1,24 +1,26 @@
 def capitalize(name):
-   name = "jadeja baptiste"
-   x = name.title()
+   name_1 = name
+   x = name_1.title()
    print(x)
        
-capitalize("jadeja baptiste")
+capitalize("chris evans")
+capitalize("bucky barnes")
 
 def init(name):
-    name = "jadeja baptiste"
-    new = name[0]
+    name_1 = name
+    new = name_1[0]
+    new_2 = name_1[-8:]
     x = new.title()
-    print(x + "." + " " + "Baptiste")
-
-init("jadeja baptiste")
+    y = new_2.title()
+    return x + "." + " " + y
+    
+print(init("jadeja baptiste"))
 
 def part_pig_latin(name):
-    name = "jadeja"
-    new = name[-5:]
-    print(new + "j" + "ay")
+    x = name[1:]
+    return x + name[0] + "ay"
 
-part_pig_latin("jadeja")
+print(part_pig_latin("jadeja"))
     
     
 
