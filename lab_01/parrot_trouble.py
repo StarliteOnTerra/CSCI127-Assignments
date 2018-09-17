c70bdab70fac422f4c2ddb9dcea4c84553968f35
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-09-17 00:16:17 -0400

    Add program.py

--- lab_01/parrot_trouble.py
+++ lab_01/parrot_trouble.py
@@ -1,3 +1,2 @@
+print "Hello World!"
 
-def parrot_trouble(talking, hour):
-    return (talking and (hour < 7 or hour > 20))
---
+++ program.py
@@ -0,0 +1,24 @@
+def capitalize(name):
+   name = "jadeja baptiste"
+   x = name.title()
+   print(x)
+       
+capitalize("jadeja baptiste")
+
+def init(name):
+    name = "jadeja baptiste"
+    new = name[0]
+    x = new.title()
+    print(x + "." + " " + "Baptiste")
+
+init("jadeja baptiste")
+
+def part_pig_latin(name):
+    name = "jadeja"
+    new = name[-5:]
+    print(new + "j" + "ay")
+
+part_pig_latin("jadeja")
+    
+    
+
