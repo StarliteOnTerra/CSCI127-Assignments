124045abc8ba5a8dc1daa414a725f752c4c055e6
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-09-24 09:16:51 -0400

    add fizzbuzz.py

---
+++ lab_02/fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(max_value):
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 00:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+        i = i + 1
+print(fizzbuzz(100))
+  
