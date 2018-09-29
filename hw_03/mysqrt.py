cf8fa9075a9265d4de59da1f4261e5bbcc58bb4a
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-09-29 00:29:34 -0400

    add mysqrt.py

---
+++ hw_03/mysqrt.py
@@ -0,0 +1,15 @@
+# Jadeja Baptiste and Christopher He
+def mysqrt(n):
+      guess = n / 2
+      old_guess = guess
+      while guess ** 2 != n:
+        guess = (guess + (n/guess)) / 2
+        if old_guess == guess:
+            return guess
+        old_guess = guess
+      return guess
+
+for i in range(1,101):
+    print(i, mysqrt(i))
+
+ 
