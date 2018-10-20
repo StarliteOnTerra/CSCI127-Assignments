0b0bb9a670ba954a7408114a85617fb783b754e1
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-10-20 04:09:58 -0400

    add exam_01

--- hw_05/hw_05.py
+++ hw_05/hw_05.py
@@ -1,17 +1,18 @@
+l = [3, 4, 5, 6, 7, 8, 9, 10, 11]
 def filterodd(l):
-    l = [3, 4, 5, 6, 7, 8, 9, 10, 11]
     for x in l:
         if x % 2 != 1:
             continue
         print(x)            
-print(filterodd(1))
+print(filterodd(l))
 
+l = [2, 3, 4, 5, 6]
 def mapsquare(l):
-    l = [1, 2, 3, 4, 5]
+    l = [2, 3, 4, 5, 6]
     for x in l:
         x = x ** 2
         print(x)
-print(mapsquare(1))
+print(mapsquare(l))
 
 
             
