80966abc00ba8c1dbe81c60122a8fc82c25ce4d0
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-11-19 09:32:28 -0500

    add exam_02

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,6 +1,17 @@
 l = []
 def encode(s):
     letters = s.split
+    l.append(letters)
+    return l
+
+print(encode("abbbaaacddcc"))
+
+def count(s):
+    for letter in l:
+        list = l.count(letter)
+        l.append(letter)
+    return l
+    
     
     
         
