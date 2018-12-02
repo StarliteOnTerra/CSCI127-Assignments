f46bc82659803ebcb64afaa0c2b1e799ee9625d4
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-12-02 00:25:55 -0500

    add hw_09

--- hw_09/guess.cpp
+++ hw_09/guess.cpp
@@ -1,22 +1,21 @@
-#include <cstdlib>
 #include <iostream>
 
 int main() {
     int userNumber;
     std::cout << "Enter an integer value between 0 and 99: ";
     std::cin >> userNumber;
-
-    int guess = rand();
-    std::cout << guess;
+    int guess;
     while(guess <= 99) {
+    	int i = rand() % 100; /* between 0 and 99 */
+    	std::cout << i;
     	std::cout << " is this correct?";
 
     	int number_1;
     	int number_2;
     	int number_3;
-    		if(userNumber < guess) { std::cin >> number_1; } 
-    		if(userNumber > guess) { std::cin >> number_2; }
-    		if(userNumber == guess) { std::cin >> number_3; std::cout << "This is correct."; }
+    		if(guess < userNumber) { std::cin >> number_1; } 
+    		if(guess > userNumber) { std::cin >> number_2; }
+    		if(guess == userNumber) { std::cin >> number_3; std::cout << "This is correct."; }
     }
     return 0;
 }
