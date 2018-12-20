5e6c72312806427d4398f81b6450786ca2012ac5
Jadeja Baptiste <starlight@Jadejas-MacBook-Air.local>
2018-12-20 11:02:20 -0500

    add final_2

--- final_2/dict.py
+++ final_2/dict.py
@@ -8,9 +8,12 @@ def addline(d, line):
             d[first] = newstring
         if first in string:
             d[newstring] = d[newstring] + 1
-            
     return d
 print(addline(d, "This Is Me"))
+print(addline(d, "Walking On Sunshine"))
+print(addline(d, "The Only Thing To Do Is Fight"))
+print(addline(d, "She Sells Seashells By The Seashore"))
+print(addline(d, "By Me Icecream))
             
             
     
